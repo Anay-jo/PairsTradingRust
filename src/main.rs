c972c671125a2bae743bdacc16c5dece2388d29b
Anay <anay.jo@gmail.com>
2024-11-07 22:24:33 -0600

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+use fmp::Client;
+use fmp::period::FMPPeriod;
+
+
+
+fn main() {
+    let data = Client::new(
+        "https://financialmodelingprep.com/api",
+        "/v3/historical-price-full/AAPL",
+    );
+    
+    
+}
+
+
