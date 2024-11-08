ce062f844f0a84fc799b9078f65f4189a5193bad
Anay <anay.jo@gmail.com>
2024-11-08 01:16:27 -0600

    Got Ticker and input to work. Trying to find good trend similarity algorithm

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,63 @@ use fmp::Client;
 use fmp::period::FMPPeriod;
 use fmp::historical_price::*;
 use reqwest::StatusCode;
+use std::env;
+use std::io;
+use std::thread;
+use std::io::Write;
 
 
 #[tokio::main]
 async fn main() {
-    res().await;
+    println!("Enter ticker symbol here: ");
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input);
+
+    let mut data: Vec<FMPHistoricalPrice> = init_data(&input).await;
+    let mut currdata = Vec::new(); //present block
+    for i in 0..90 {
+        currdata.push(data[i].change_percent);
+    }
+    let x = data.len()%90;
+    for i in 0..x {
+        data.pop();
+    }
+    let mut min_diff:f64 = -1.0;
+    let mut min_idx:usize = 0;
+    let mut i = 90;
+    while i + 90 <= data.len() {
+        let mut temp_vec = Vec::new();
+        for j in i..i+90 {
+            temp_vec.push(data[j].change_percent);
+        }
+        let temp_val = calculate_diff(&temp_vec, &currdata);
+        if min_diff < 0.0 || temp_val < min_diff {
+            min_diff = temp_val;
+            min_idx = i;
+        }
+        i+=1;
+    }
+    println!("{}", data[min_idx].date);
 }
 
-async fn init_data() -> Client {
+async fn init_data(ticker: &str) -> Vec<FMPHistoricalPrice> {
     let data = Client::new(
         "https://financialmodelingprep.com/api",
         "0mIYdxrV89xwrkb99a8zdcElTiQu44sn"
     );
-    return data;
-}
-
-async fn res(){
-    let data = init_data();
-    let res: Result<Vec<FMPHistoricalPrice>, StatusCode> = data.await.historical_prices("AAPL").await;
+    let res: Result<Vec<FMPHistoricalPrice>, StatusCode> = data.historical_prices(ticker).await;
     let vec: Vec<FMPHistoricalPrice> = res.unwrap();
-    for i in 0..vec.len() {
-        println!("{}", vec[i].change_percent);
-    }
-
+    println!("Change in percent during {} was {}%", vec[0].date, vec[0].change_percent);
+    vec
 }
 
 
+
+//calculates trend difference
+fn calculate_diff(vec1: &Vec<f64>, vec2: &Vec<f64>) -> f64 {
+    let mut sum: f64 = 0.0;
+    for i in 0..90 {
+        sum += vec1[i] - vec2[i];
+    } 
+    sum * sum
+}
